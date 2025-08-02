49ec7e885f366f01ff83e876f4ea16ca6c3f0155
Dipesh Sakharkar <dipeshsakharkar1@gmail.com>
2025-08-02 14:29:50 +0530

    added get airplane api

--- src/routes/v1/airplane-routes.js
+++ src/routes/v1/airplane-routes.js
@@ -5,8 +5,15 @@ const {airplaneMiddleware}  = require('../../middlewares')
 
 const router = express.Router();
 
+// api/v1/airplanes POST
 router.post('/',airplaneMiddleware.validateCreateRequest, AirplaneController.createAirplane);
+
+// api/v1/airplanes/:id GET
 router.get('/', AirplaneController.getAirplanes);
+
+// api/v1/airplanes GET
+router.get('/:id', AirplaneController.getAirplane);
+
 
 
 module.exports = router;
--- src/services/airplane-service.js
+++ src/services/airplane-service.js
@@ -9,7 +9,6 @@ async function createAirplane(data){
     const airplane = await airplaneRepository.create(data);
     return airplane;
    }catch(error){
-    console.log(error)
     if(error.name === 'SequelizeValidationError'){
       let explanation = [];
       error.errors.forEach((err)=>{
@@ -31,7 +30,21 @@ async function getAirplanes(){
    }
 }
 
+async function getAirplane(id){
+   try{
+     const airplane = await airplaneRepository.get(id);
+     return airplane;
+   }catch(error){
+      if(error.statusCode == StatusCodes.NOT_FOUND){
+     throw new AppError('Airplane you requested is not present',error.statusCode)
+
+      }
+     throw new AppError('Cannot fetch data for this airplane',StatusCodes.INTERNAL_SERVER_ERROR)
+   }
+}
+
 module.exports ={
     createAirplane,
-    getAirplanes
+    getAirplanes,
+    getAirplane
 }
