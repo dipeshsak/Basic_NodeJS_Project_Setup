e59b4fee8e3658e5cd1b686d66f76e35779d49c8
Dipesh Sakharkar <dipeshsakharkar1@gmail.com>
2025-08-10 23:23:22 +0530

    filtering & sorting on Flights

--- src/routes/v1/flight-routes.js
+++ src/routes/v1/flight-routes.js
@@ -8,5 +8,8 @@ const router = express.Router();
 // api/v1/flights POST
 router.post('/',FlightMiddleware.validateCreateRequest, FlightController.createFlight);
 
+// api/v1/flights GET
+router.get('/', 
+        FlightController.getAllFlights);
 
 module.exports = router;
